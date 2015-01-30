756606923cf3a82108d0bf8d6668705b53f5c31c
Brian Ashburn <ashburn.brian@gmail.com>
2015-01-30 13:44:18 -0500

    Adding fsi prefix to aliases to reduce any conflict

--- installation_build/installation/alias/demoalias.sh
+++ installation_build/installation/alias/demoalias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-alias fusedir="cd @@rhq.deploy.dir@@/jboss-fuse-6.1.0.redhat-379/bin"
-alias derbydir="cd @@rhq.deploy.dir@@/db-derby-10.10.2.0-bin/bin"
+alias fsifusedir="cd @@rhq.deploy.dir@@/jboss-fuse-6.1.0.redhat-379/bin"
+alias fsiderbydir="cd @@rhq.deploy.dir@@/db-derby-10.10.2.0-bin/bin"
